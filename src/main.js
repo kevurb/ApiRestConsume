f2b25a629e40aee00912fec078c47de5830b8219
unknown <kevin.kostone@gmail.com>
2024-10-02 23:46:44 -0500

    add env}

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 
 const API_URL_TRENDING = 'https://api.themoviedb.org/3/trending/movie/week?api_key='+ API_KEY
 const API_URL_GENRES = 'https://api.themoviedb.org/3/genre/movie/list?api_key='+ API_KEY
-
+const API_KEY_GITHUB = process.env.API_KEY;
 async function getTrendingMoviesPreview() {
     const res = await fetch(API_URL_TRENDING);
     const data = await res.json();
